6ea2d1da0218bd987f368cdff49ebc8b2bfbf8e2
muhammedashfaq <123805225+muhammedashfaq@users.noreply.github.com>
2024-01-22 14:25:15 +0530

    'added'

---
+++ Students Card/src/App.jsx
@@ -0,0 +1,275 @@
+import { useEffect, useState } from 'react';
+import data from './Data'
+
+import StudentCard from './StudentCard';
+function App() {  
+
+const [newData,setNewData] =useState([])
+  const [showModal,setShowModal]=useState(false)
+  const [selectedStudentId, setSelectedStudentId] = useState(null);
+  const [formData, setFormData] = useState({
+    studentName: '',
+    language1: '',
+    english: '',
+    maths: '',
+    science: '',
+    social: '',
+    class:""
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [name]: value,
+    }));
+  }
+  const handleClose = () => {
+    setShowModal(false)
+    setSelectedStudentId(null)
+  }
+
+  const activeModal=(id)=>{
+    setShowModal(true)
+    setSelectedStudentId(id)
+  }
+
+  const handleSave = () => {
+    const uniqueId = new Date().getTime().toString()
+    const newData = { id: uniqueId, ...formData };
+    const existingData = JSON.parse(localStorage.getItem('students')) || [];
+    const updatedData = [...existingData, newData];
+    localStorage.setItem('students', JSON.stringify(updatedData));
+  
+    setFormData({
+      studentName: '',
+      language1: '',
+      english: '',
+      maths: '',
+      science: '',
+      social: '',
+      class:""
+    });
+  };
+  
+  useEffect(()=>{
+    const newdata =localStorage.getItem("student")
+    if (newdata) {
+      setNewData(JSON.parse(newdata));
+    }
+  },[data])
+
+
+
+  return (
+    <>
+<div className=' m-10'>
+    <button onClick={()=>localStorage.clear("students")}>clos</button>
+    <div className='w-full flex justify-center items-center'>
+    <div className="w-max h-max border-solid border-2 p-8 border-sky-500 grid grid-cols-2 gap-4">
+  <div>
+    <div className="space-y-4 text-sm">
+      <div>
+        <label htmlFor="studentName" className="block dark:text-gray-400">
+          Student Name
+        </label>
+        <input
+          type="text"
+          name="studentName"
+          id="studentName"
+          placeholder="Student Name"
+          onChange={handleChange}
+          className="w-full px-4 py-3 rounded-md dark:border-gray-700 dark:bg-gray-900 dark:text-gray-100 focus:dark:border-violet-400"
+        />
+      </div>
+
+      <div>
+        <label htmlFor="username2" className="block dark:text-gray-400">
+        Language 1
+        </label>
+        <input
+          name="language1"
+          id="username2"
+          placeholder="Language 1"
+          onChange={handleChange}
+          required
+          type="number"
+          className="w-full px-4 py-3 rounded-md dark:border-gray-700 dark:bg-gray-900 dark:text-gray-100 focus:dark:border-violet-400"
+        />
+      </div>
+      <div>
+        <label htmlFor="username3" className="block dark:text-gray-400">
+        English
+        </label>
+        <input
+          name="english"
+          id="username3"
+          placeholder="English"
+          onChange={handleChange}
+          required
+          type="number"
+          className="w-full px-4 py-3 rounded-md dark:border-gray-700 dark:bg-gray-900 dark:text-gray-100 focus:dark:border-violet-400"
+        />
+      </div>
+    </div>
+  </div>
+
+  <div>
+  <div className="space-y-4 text-sm">
+      <div>
+        <label htmlFor="studentName" className="block dark:text-gray-400">
+        Maths
+        </label>
+        <input
+        required
+        type="number"
+          name="maths"
+          id="studentName"
+          placeholder="Maths"
+          onChange={handleChange}
+          className="w-full px-4 py-3 rounded-md dark:border-gray-700 dark:bg-gray-900 dark:text-gray-100 focus:dark:border-violet-400"
+        />
+      </div>
+      <div>
+        <label htmlFor="username1" className="block dark:text-gray-400">
+        Science
+        </label>
+        <input
+          required
+          type="number"
+          name="science"
+          id="username1"
+          placeholder="Science"
+          onChange={handleChange}
+          className="w-full px-4 py-3 rounded-md dark:border-gray-700 dark:bg-gray-900 dark:text-gray-100 focus:dark:border-violet-400"
+        />
+      </div>
+      <div>
+        <label htmlFor="username2" className="block dark:text-gray-400">
+        Social
+        </label>
+        <input
+         required
+         type="number"
+          name="social"
+          id="username2"
+          placeholder="Social"
+          onChange={handleChange}
+          className="w-full px-4 py-3 rounded-md dark:border-gray-700 dark:bg-gray-900 dark:text-gray-100 focus:dark:border-violet-400"
+        />
+      </div>
+      <div>
+        <label htmlFor="username2" className="block dark:text-gray-400">
+        Class
+        </label>
+        <input
+         required
+         type="text"
+          name="class"
+          id="username2"
+          placeholder="Class"
+          onChange={handleChange}
+          className="w-full px-4 py-3 rounded-md dark:border-gray-700 dark:bg-gray-900 dark:text-gray-100 focus:dark:border-violet-400"
+        />
+      </div>
+
+    </div>
+  </div>
+
+  <div className="col-span-2">
+    <button
+      type="button"
+      onClick={handleSave}
+      className="w-full px-8 py-3 font-semibold rounded-md dark:bg-violet-400 dark:text-gray-900 mt-4"
+    >
+      ADD
+    </button>
+  </div>
+</div>
+
+
+
+  
+      </div>
+  <div className="h-screen flex justify-center items-center">
+  <div className="container p-2 mx-auto sm:p-4 dark:text-gray-100">
+	<h2 className="mb-4 text-2xl font-semibold leadi">STUDENTS FROM Xth STD </h2>
+	<div className="overflow-x-auto">
+		<table className="min-w-full text-xs">
+		
+			<thead className="dark:bg-gray-700">
+				<tr className="text-left">
+					<th className="p-3">SL NO </th>
+					<th className="p-3">Sudent Name</th>
+          <th className="p-3">Class</th>
+          <th className="p-3">Language 1</th>
+					<th className="p-3">English</th>
+					<th className="p-3">Maths</th>
+					<th className="p-3">Science</th>
+					<th className="p-3">Social</th>
+          <th className="p-3">Status</th>
+				</tr>
+			</thead>
+			<tbody>
+
+				{data.map((student,i)=>(
+
+          
+          <tr key={i} className="border-b border-opacity-20 dark:border-gray-700 dark:bg-gray-900">
+					<td className="p-3">
+						<p>{i+1}</p>
+					</td>
+					<td className="p-3">
+						<p>{student.name}</p>
+            </td>
+            <td className="p-3">
+          <p>{student.class}</p>
+					</td>
+					<td className="p-3">
+          <p>{student.Language1}</p>
+					</td>
+          <td className="p-3">
+          <p>{student.English}</p>
+					</td>
+          <td className="p-3">
+          <p>{student.Maths}</p>
+					</td>
+					<td className="p-3">
+          <p>{student.Science}</p>
+            </td>
+            <td className="p-3 ">
+						<p>{student.Social}</p>
+            </td>
+            <td className="p-3 ">
+              <button onClick={()=>activeModal(student.id)}>
+                
+						<span className="px-3 py-1 font-semibold rounded-md dark:bg-violet-400 dark:text-gray-900">
+            <span>VIEW</span>
+						</span>
+              </button>
+            </td>
+            </tr>
+            
+            
+         ))
+        }
+		
+			</tbody>
+		</table>
+	</div>
+</div>
+
+{showModal && (
+          <StudentCard onClose={handleClose} visible={showModal} selectedStudentId={selectedStudentId} />
+        )
+      }
+    
+  </div>
+  
+      </div>
+  
+    </>
+  )
+}
+
+export default App
---
+++ Students Card/src/StudentCard.jsx
@@ -0,0 +1,64 @@
+import data from './Data'
+const StudentCard = ({onClose, visible,selectedStudentId}) => {
+
+	const details = data.find((item)=>item.id === selectedStudentId)
+	const subjects = Object.keys(details).filter((key) => key !== "id" && key !== "name" && key !== "class");
+	const marks = subjects.map((subject) => details[subject]);
+	const totalMark = marks.reduce((acc, mark) => acc + mark, 0)
+	const averageMark = totalMark / subjects.length;
+	const highestMark = Math.max(...marks);
+	const lowestMark = Math.min(...marks);
+
+
+    if (!visible) return null
+  return (
+    <div id="container"
+        className=" fixed inset-0 backdrop-blur-xl w-screen h-screen  flex justify-center items-center z-50 transition ease-in-out duration-700"  >
+		  <div className="flex flex-col max-w-md p-6 space-y-4 divide-y sm:w-96 sm:p-10 dark:divide-gray-700 dark:bg-gray-900 dark:text-gray-100">
+         <button onClick={onClose}><span class="material-symbols-outlined">
+close
+</span></button>
+
+
+<div className='flex justify-between'> 
+
+	<h2 className="text-2xl font-semibold">{details.name}</h2>
+	<h2 className="text-2xl font-semibold">Class : {details.class}</h2>
+</div>
+	
+	<div className="pt-4 space-y-2">
+		<div>
+			<div className="flex justify-between">
+				<span>Total mark</span>
+				<span>{totalMark}</span>
+			</div>
+			
+		</div>
+		<div className="flex justify-between">
+			<span>Avarage Mark</span>
+			<span>{averageMark}</span>
+		</div>
+	</div>
+
+	<div className="pt-4 space-y-2">
+		<div className="flex justify-between">
+			<span>Highest Mark</span>
+			<span>{highestMark}</span>
+		</div>
+		<div className="flex flex-col">
+			<div className="flex justify-between">
+				<span>Lowest mark</span>
+				<span>{lowestMark}</span>
+			</div>
+		</div>
+	
+	</div>
+</div>
+         
+
+
+    </div>
+  )
+}
+
+export default StudentCard
